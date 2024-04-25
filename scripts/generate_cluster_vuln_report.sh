8a9b2374635ec3db5aea4e2a6db7b0cf4251ef3b
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2024-04-25 11:32:11 +0100

    fix: 🐛 alpine jq throws syntax errors that local doesn't

--- scripts/generate_cluster_vuln_report.sh
+++ scripts/generate_cluster_vuln_report.sh
@@ -9,12 +9,12 @@ echo "Getting all vulnerabilities for ${CLUSTER_NAME}..."
 kubectl get vulnerabilityreports.aquasecurity.github.io -A -o json > ${CLUSTER_NAME}_vuln.json
 
 echo "Getting namespace annotations to enrich vulnerability report..."
-jq -r '.items|map(.metadata.namespace)|unique.[]' ${CLUSTER_NAME}_vuln.json | xargs -n 1 | xargs -I % bash -c 'kubectl get ns % -ojson > %.json'
+jq -r '.items | map(.metadata.namespace) | unique | .[]' ${CLUSTER_NAME}_vuln.json | xargs -n 1 | xargs -I % bash -c 'kubectl get ns % -ojson > %.json'
 
 cp ${CLUSTER_NAME}_vuln.json updated.json
 
 echo "Looping over vulnerabilities and enriching with relevant namespace details..."
-jq -c '.items.[]' ${CLUSTER_NAME}_vuln.json | while read i; do
+jq -c '.items | .[]' ${CLUSTER_NAME}_vuln.json | while read i; do
   OBJ_UID=$(echo $i | jq -r '.metadata.uid')
   NS=$(echo $i | jq -r '.metadata.namespace')
 
@@ -23,11 +23,11 @@ jq -c '.items.[]' ${CLUSTER_NAME}_vuln.json | while read i; do
   TEAM_NAME=$(jq -r '.metadata.annotations."cloud-platform.justice.gov.uk/team-name"' "$NS.json")
 
   # add the new values to the object
-  jq --arg SOURCE_CODE "$SOURCE_CODE" --arg OBJ_UID "$OBJ_UID" '{items: [.items.[] | select(.metadata.uid | contains($OBJ_UID)) += {"cloud-platform.justice.gov.uk/source-code": $SOURCE_CODE }]}' updates_here.json > updated_source_code.json
+  jq --arg SOURCE_CODE "$SOURCE_CODE" --arg OBJ_UID "$OBJ_UID" '{items: [.items | .[] | select(.metadata.uid | contains($OBJ_UID)).metadata += {"cloud-platform.justice.gov.uk/source-code": $SOURCE_CODE }]}' updated.json > updated_source_code.json
 
-  jq --arg OWNER "$OWNER" --arg OBJ_UID "$OBJ_UID" '{items: [.items.[] | select(.metadata.uid | contains($OBJ_UID)) += {"cloud-platform.justice.gov.uk/owner": $OWNER }]}' updated_source_code.json > updated_owner.json
+  jq --arg OWNER "$OWNER" --arg OBJ_UID "$OBJ_UID" '{items: [.items | .[] | select(.metadata.uid | contains($OBJ_UID)).metadata += {"cloud-platform.justice.gov.uk/owner": $OWNER }]}' updated_source_code.json > updated_owner.json
 
-  jq --arg TEAM_NAME "$TEAM_NAME" --arg OBJ_UID "$OBJ_UID" '{items: [.items.[] | select(.metadata.uid | contains($OBJ_UID)) += {"cloud-platform.justice.gov.uk/team-name": $TEAM_NAME }]}' updated_owner.json > updated_team_name.json
+  jq --arg TEAM_NAME "$TEAM_NAME" --arg OBJ_UID "$OBJ_UID" '{items: [.items | .[] | select(.metadata.uid | contains($OBJ_UID)).metadata += {"cloud-platform.justice.gov.uk/team-name": $TEAM_NAME }]}' updated_owner.json > updated_team_name.json
 
   cp updated_team_name.json updated.json
 done
